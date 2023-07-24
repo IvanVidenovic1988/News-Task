2de0a291f5e6617875c9ca2ecc444406ee9217a4
IvanVidenovic1988 <mailivan019@gmail.com>
2023-07-24 17:50:05 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex-center">
+      <h1 className="text-4xl text-red-700">kurac</h1>
+      <p className="text-4xl text-yellow-200">kita</p>
+    </div>
+  );
+}
+
+export default App;
